2e780bb438f00595a925a3aadb82e9cc2094043a
Nassim Attemani <nassim.attem@hotmail.com>
2021-11-24 21:22:56 +0100

    need to npm install swiper

--- app/javascript/components/swiper.js
+++ app/javascript/components/swiper.js
@@ -1,13 +1,22 @@
 import Swiper from 'swiper';
 
-// import 'swiper/swiper.scss';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+
 
 const initSwiper = () => {
   if (document.querySelector(".mySwiper")) {
+    Swiper.use([Navigation, Pagination]);
     const swiper = new Swiper(".mySwiper", {
       slidesPerView: 1,
       spaceBetween: 30,
       loop: true,
+      autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
       pagination: {
         el: ".swiper-pagination",
         clickable: true,
