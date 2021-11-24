d86aa95f46afa8986036cd02410865ae8a47386b
Nassim Attemani <nassim.attem@hotmail.com>
2021-11-24 12:16:15 +0100

    init controller bookings

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :cars
+  resources :cars do
+    # resources :bookings, only: [ :new, :create]
+  end
   get 'allmycars', to: 'cars#mycars'
 end
