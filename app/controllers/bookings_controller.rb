026a40e8ed1558a860d1a5390a42ddd2efc4e01c
Nassim Attemani <nassim.attem@hotmail.com>
2021-11-24 12:28:07 +0100

    routes new x create done, destroy to do, view for new and create to do

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,23 @@
 class BookingsController < ApplicationController
+   def create
+    @car = Car.find(params[:car_id])
+    @booking = Booking.new(bookings_params)
+    @booking.list = @list
+    if @booking.save
+      redirect_to car_path(@car)
+    else
+      render :new
+    end
+  end
+
+  def new
+    @car = Car.find(params[:car_id])
+    @booking = Booking.new
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:booking).permit(:start_date, :end_date, :status)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :cars do
-    # resources :bookings, only: [ :new, :create]
+    resources :bookings, only: [ :new, :create]
   end
   get 'allmycars', to: 'cars#mycars'
 end
