34f74ba25e9c9e6927878e06bed3aa8285c2a926
Nassim Attemani <nassim.attem@hotmail.com>
2021-11-24 14:57:39 +0100

    create booking works when u'r log

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,8 +2,10 @@ class BookingsController < ApplicationController
    def create
     @car = Car.find(params[:car_id])
     @booking = Booking.new(bookings_params)
-    @booking.list = @list
-    if @booking.save
+    @user = current_user
+    @booking.car = @car
+    @booking.user = @user
+    if @booking.save!
       redirect_to car_path(@car)
     else
       render :new
@@ -15,9 +17,14 @@ class BookingsController < ApplicationController
     @booking = Booking.new
   end
 
+  def destroy
+    @booking.destroy
+    redirect_to root, notice: 'Garden was successfully destroyed.'
+  end
+
   private
 
-  def bookmark_params
+  def bookings_params
     params.require(:booking).permit(:start_date, :end_date, :status)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :cars do
-    resources :bookings, only: [ :new, :create]
+    resources :bookings, only: [:new, :create]
   end
+  resources :bookings, only: [:destroy]
   get 'allmycars', to: 'cars#mycars'
 end
