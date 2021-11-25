4f61d1c9f4211d54a55a4d683a61a76f3974d609
Nassim Attemani <nassim.attem@hotmail.com>
2021-11-25 17:08:40 +0100

    car x user works with new columns, need to check the bookings table

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,6 +1,6 @@
 class Car < ApplicationRecord
   belongs_to :user
-  validates :brand, :price_per_day, :localisation, :immatriculation, :details, :photos,  presence: true
+  validates :brand, :price_per_day, :localisation, :immatriculation, :details, :model, :photos,  presence: true
   has_many :bookings
   has_many_attached :photos
 end
