1e4fa0d4f529fa2c669848d1415ddb4df79ca78d
thomas billiau <billiau.thomas@gmail.com>
2021-11-25 12:45:31 +0100

    footer finie

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -2,7 +2,7 @@ const scroll = () => {
   const navbar = document.querySelector(".header");
   if (navbar) {
     document.addEventListener('scroll', (event) => {
-      navbar.style.backgroundColor = "rgba(0,0,0,0.4)";
+      navbar.style.backgroundColor = "rgba(244, 244, 244)";
     });
   };
 };
