6d4754721e17e5f6a7ebb825e67ac55d425f54d1
Nassim Attemani <nassim.attem@hotmail.com>
2021-11-26 12:38:53 +0100

    wip

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,6 +1,7 @@
 class Car < ApplicationRecord
   belongs_to :user
   validates :brand, :price_per_day, :localisation, :immatriculation, :details, :model, :photos,  presence: true
+  validates :photos, length: { :minimum 2 }
   has_many :bookings
   has_many_attached :photos
 end
