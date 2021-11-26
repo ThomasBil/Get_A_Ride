8fee1db449024c44454f40723ab620ea867fb16c
Nassim Attemani <nassim.attem@hotmail.com>
2021-11-26 15:47:41 +0100

    debug

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_many :cars, dependent: :destroy
-  has_many :bookings
+  has_many :bookings, dependent: :destroy
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
